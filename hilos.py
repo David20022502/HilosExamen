4a1d47666456d3aba549918f674ade3e5d0637b3
David <cacuangodarwin1999@gmail.com>
2022-07-12 19:09:13 -0500

    prueba de hilos

--- hilos.py
+++ hilos.py
@@ -5,8 +5,8 @@ def mainFunction(name):
     print("hola que tal "+name)
     time.sleep(5)
 
-if __name__ == '__mian__':
-    thread = threading.Thread(target=mainFunction,args=("Codi",))
+if __name__ == '__main__':
+    thread = threading.Thread(target=mainFunction,args=("David Cacuango",))
     thread.start()
     thread.join()
     print("hola, un saludo desde el hilo principal del còdigo")
